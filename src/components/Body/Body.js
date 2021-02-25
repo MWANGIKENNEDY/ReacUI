ad24609d9ab8cd4e6e20966ea9c08e9c5cf36990
MWANGIKENNEDY <kenmwan75@gmail.com>
2021-02-25 11:05:52 +0300

    Created project structure

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Body.module.css';
+
+const BodyComponent=()=>{
+    return (
+        <div>
+            body
+        </div>
+    )
+}
+
+export default BodyComponent;
---
+++ src/components/Navigation/Navigationbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navigationbar.module.css';
+
+const NavigationComponent=()=>{
+    return (
+        <div>
+            navigation
+        </div>
+    )
+}
+
+export default NavigationComponent;
